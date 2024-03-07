cee10895ac96cef141279881dbba40afa5fe7c9a
Jitendra Choudhary <jitendrachoudhary7087@gmail.com>
2024-03-07 21:53:37 +0530

    Login

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import Navbar from "./components/Common/Navbar";
+import OpenRoute from "./components/core/Auth/OpenRoute"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import About from "./pages/About"
+//import Catalog from "./pages/Catalog"
+import Contact from "./pages/Contact"
+//import CourseDetails from "./pages/CourseDetails"
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePaaword";
+import VerifyEmail from "./pages/VerifyEmail";
+
+function App() {
+  return (
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="courses/:courseId" element={<CourseDetails />} /> */}
+        {/* <Route path="catalog/:catalogName" element={<Catalog />} /> */}
+
+
+
+
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        /> 
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        /> 
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
